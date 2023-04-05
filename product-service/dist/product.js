10b18f28170667bc55f1805b1bde9098d60f35d5
Ugochukwu Amajor <maximmajor4us@gmail.com>
2023-04-05 11:23:58 +0100

    settting up product service

---
+++ product-service/dist/product.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const tslib_1 = require("tslib");
+const mongoose_1 = tslib_1.__importDefault(require("mongoose"));
+const Schema = mongoose_1.default.Schema;
+const ProductSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+}, { timestamps: true });
+const product = mongoose_1.default.model("Product", ProductSchema);
+exports.default = product;
---
+++ product-service/src/models/product.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const ProductSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const product = mongoose.model("Product", ProductSchema);
+export default  product 
